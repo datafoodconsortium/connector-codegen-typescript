857080ad0c005873e5189dd18481d9875ab3d263
Maxime Lecoq <maxime@lecoqlibre.fr>
2023-03-18 16:36:40 +0100

    Before regenerate tests

---
+++ static/test/Connector.test.js
@@ -0,0 +1,25 @@
+import Connector from "../lib/Connector.js";
+import measures from '../test/thesaurus/measures.json' assert { type: 'json' };
+
+const connector = new Connector();
+//connector.loadMeasures(measures);
+const expected = ``;
+
+test('Connector:test', async () => {
+    try {
+        //console.log(JSON.stringify(measures));
+        /*const context = {
+            "dfc-m": "http://static.datafoodconsortium.org/data/measures.rdf#",
+            "skos": "http://www.w3.org/2004/02/skos/core#",
+            "rdf": "http://www.w3.org/2000/01/rdf-schema#"
+        }
+
+        const imported = await connector.import(JSON.stringify(measures), { context: context });
+        console.log(imported[0]);*/
+        //console.log(measures["@graph"]);
+        //console.log(connector);
+        await connector.loadMeasures(JSON.stringify(measures));
+        console.log(connector.MEASURES.UNIT.CURRENCYUNIT.AUSTRALIANDOLLAR)
+    }
+    catch(e) { console.log(e) }
+});
