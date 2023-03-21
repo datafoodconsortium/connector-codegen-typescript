3070f1219e76ef669a29330276f21a9f2120642f
Maxime Lecoq <maxime@lecoqlibre.fr>
2023-03-21 23:58:42 +0100

    Add tests

--- static/test/QuantitativeValue.test.js
+++ static/test/QuantitativeValue.test.js
@@ -7,27 +7,48 @@ await connector.loadMeasures(JSON.stringify(measures));
 
 const kilogram = connector.MEASURES.UNIT.QUANTITYUNIT.KILOGRAM;
 
-const data = ``;
+const quantitativeValue = new QuantitativeValue({ 
+    connector: connector, 
+    quantity: 1, 
+    unit: kilogram 
+});
+
+const json = ``;
 
 test('QuantitativeValue:import', async () => {
-    const quantitativeValue = new QuantitativeValue({ 
-        connector: connector, 
-        quantity: 1, 
-        unit: kilogram 
-    });
-    
-    const imported = (await connector.import(data))[0];
+    const importedAll = await connector.import(json);
+    const imported = importedAll[0];
+    expect(importedAll.length).toStrictEqual(1);
     expect(imported.equals(quantitativeValue)).toStrictEqual(true);
 });
 
 test('QuantitativeValue:export', async () => {
-    const quantitativeValue = new QuantitativeValue({ 
-        connector: connector, 
-        quantity: 1, 
-        unit: kilogram 
-    });
-    
     const serialized = await connector.export([quantitativeValue]);
     console.log(serialized);
-    expect(serialized).toStrictEqual(data);
+    expect(serialized).toStrictEqual(json);
+});
+
+test('QuantitativeValue:getSemanticId', async () => {
+    expect(quantitativeValue.getSemanticId()).toStrictEqual(undefined);
+});
+
+test('QuantitativeValue:getQuantityUnit', async () => {
+    const expected = await quantitativeValue.getQuantityUnit();
+    expect(expected.equals(kilogram)).toStrictEqual(true);
+});
+
+test('QuantitativeValue:getQuantityValue', async () => {
+    expect(quantitativeValue.getQuantityValue()).toStrictEqual(1);
+});
+
+test('QuantitativeValue:setQuantityUnit', async () => {
+    const gram = connector.MEASURES.UNIT.QUANTITYUNIT.GRAM;
+    quantitativeValue.setQuantityUnit(gram);
+    const expected = await quantitativeValue.getQuantityUnit();
+    expect(expected.equals(gram)).toStrictEqual(true);
+});
+
+test('QuantitativeValue:setQuantityValue', async () => {
+    quantitativeValue.setQuantityValue(2.5)
+    expect(quantitativeValue.getQuantityValue()).toStrictEqual(2.5);
 });
--- static/test/SaleSession.test.js
+++ static/test/SaleSession.test.js
@@ -3,23 +3,84 @@ import SaleSession from '../lib/SaleSession.js';
 import Connector from "../lib/Connector.js";
 
 const connector = new Connector();
-const expected = `{"@context":{"@vocab":"http://static.datafoodconsortium.org/ontologies/DFC_BusinessOntology.owl#"},"@id":"http://myplatform.com/saleSession1","@type":"SaleSession","beginDate":"beginDate","endDate":"endDate","quantity":"5"}`;
+
+const offer = new Offer({
+    connector: connector,
+    semanticId: "http://myplatform.com/offer1"
+});
+
+const saleSession = new SaleSession({
+    connector: connector,
+    semanticId: "http://myplatform.com/saleSession1",
+    beginDate: "beginDate",
+    endDate: "endDate",
+    quantity: 5,
+    offers: [offer]
+});
+
+const json = ``;
+
+test('SaleSession:import', async () => {
+    const importedAll = await connector.import(json);
+    const imported = importedAll[0];
+    expect(importedAll.length).toStrictEqual(1);
+    expect(imported.equals(saleSession)).toStrictEqual(true);
+});
 
 test('SaleSession:export', async () => {
-    const offer = new Offer({
-        connector: connector,
-        semanticId: "http://myplatform.com/offer1"
-    });
-
-    const saleSession = new SaleSession({
-        connector: connector,
-        semanticId: "http://myplatform.com/saleSession1",
-        beginDate: "beginDate",
-        endDate: "endDate",
-        quantity: 5,
-        offers: [offer]
-    });
-
     const serialized = await connector.export([saleSession]);
-    expect(serialized).toStrictEqual(expected);
+    console.log(serialized);
+    expect(serialized).toStrictEqual(json);
+});
+
+test('SaleSession:getSemanticId', async () => {
+    expect(saleSession.getSemanticId()).toStrictEqual("http://myplatform.com/saleSession1");
+});
+
+test('SaleSession:getBeginDate', async () => {
+    expect(saleSession.getBeginDate()).toStrictEqual("beginDate");
+});
+
+test('SaleSession:getEndDate', async () => {
+    expect(saleSession.getEndDate()).toStrictEqual("endDate");
+});
+
+test('SaleSession:getQuantity', async () => {
+    expect(saleSession.getQuantity()).toStrictEqual(5);
+});
+
+test('SaleSession:getOffers', async () => {
+    const offers = await saleSession.getOffers();
+    expect(offers.length).toStrictEqual(1);
+    expect(offers[0].equals(offer)).toStrictEqual(true);
+});
+
+test('SaleSession:setBeginDate', async () => {
+    saleSession.setBeginDate("beginDate2");
+    expect(saleSession.getBeginDate()).toStrictEqual("beginDate2");
+});
+
+test('SaleSession:setEndDate', async () => {
+    saleSession.setEndDate("endDate2");
+    expect(saleSession.getEndDate()).toStrictEqual("endDate2");
+});
+
+test('SaleSession:setQuantity', async () => {
+    saleSession.setQuantity(2.3);
+    expect(saleSession.getQuantity()).toStrictEqual(2.3);
+});
+
+test('SaleSession:addOffer', async () => {
+    const offer2 = new Offer({
+        connector: connector,
+        semanticId: "http://myplatform.com/offer2"
+    });
+    saleSession.addOffer(offer2);
+    const offers = await saleSession.getOffers();
+    expect(offers.length).toStrictEqual(2);
+    expect(offers[1].equals(offer2)).toStrictEqual(true);
+});
+
+test('SaleSession:removeOffer', async () => {
+    expect(true).toStrictEqual(false);
 });
